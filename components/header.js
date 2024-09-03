2c87a948b7aef2e1db3b829c93dd818eca54eed4
SantiagoSilva68 <167663599+SantiagoSilva68@users.noreply.github.com>
2024-09-03 16:47:19 -0300

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,23 +2,23 @@ function header(head) {
   const header = document.createElement("div");
   header.innerHTML = ` 
   <header>
-    <a href="./index.html"><img class="logo" src="./footer-images/logo.png" alt=""></a>
+    <a href="https://santiagosilva68.github.io/Desafio-Web-y-Componentes/index.html"><img class="logo" src="./footer-images/logo.png" alt=""></a>
     <div class="menu-icon" id="menuIcon">&#9776;</div>
     <nav class="nav-large">
       <ul>
-        <li class="li--menu--largo"><a href="/portfolio.html">Portfolio</a></li>
+        <a href="https://santiagosilva68.github.io/Desafio-Web-y-Componentes/portfolio.html"><li class="li--menu--largo">Portfolio</li></a>
         <li class="li--menu--largo"><a href="#servicios">Servicios</a></li>
-        <li class="li--menu--largo"><a href="/contacto.html">Contacto</a></li>
-      </ul>
+        </ul>
+        <a href="https://santiagosilva68.github.io/Desafio-Web-y-Componentes/contacto.html"><li class="li--menu--largo">Contacto</li></a>
     </nav>
   </header>
   
   <nav class="full-screen-menu" id="fullScreenMenu">
     <div class="close-icon" id="closeIcon">&times;</div>
     <ul>
-      <li class="li--menu--corto"><a href="/portfolio.html">Portfolio</a></li>
+      <a href="https://santiagosilva68.github.io/Desafio-Web-y-Componentes/portfolio.html"><li class="li--menu--corto">Portfolio</li></a>
       <li class="li--menu--corto"><a href="/index.html#servicios" id="serviciosLink">Servicios</a></li>
-      <li class="li--menu--corto"><a href="/contacto.html">Contacto</a></li>
+      <a href="https://santiagosilva68.github.io/Desafio-Web-y-Componentes/contacto.html"><li class="li--menu--corto">Contacto</li></a>
     </ul>
   </nav>
   `;
